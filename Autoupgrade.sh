3a1602e3321b2e8f57a44744706bbd7899844825
Flashbang007 <Flashbang007@gmail.com>
2019-06-28 12:57:09 +0200

    Update Autoupgrade.sh

--- Autoupgrade.sh
+++ Autoupgrade.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-PATH=$PATH:/opt/OpenPrinting-Gutenprint/sbin:/opt/OpenPrinting-Gutenprint/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/snap/bin:~/bin
+source /usr/local/lib/flashbang_lib.sh
+# Colors $COL[R,G,B,Y,LR..] for Color. $COLD for Reset
+# $SCRIPTNAME and $SCRIPTPATH for Name and Path
+# $PIPEFILE for Pushbulletervice dir is "/home/flashbang/Data/pushbullet.pipe"
+# lc_error() exits with exit information of last command of script
+# check_file() [FILE] Check if File exists and create
+# check_dir() [DIR]  Check if Directory exists and create
+# check_root() Checks if user is root and exits if not
+# check_var_empty() check if given var is empty and !exit!
+
+check_root
 
 ## Create Log? [0=no , 1=yes]
 LOG=1
@@ -10,11 +20,7 @@ MAXLOGSIZE="25600"
 # falls MAXLOGSIZE genutzt wird: Logfile rotieren(1) oder löschen(0)?
 LOGROTATE=1
 
-# -------------------------------------------------------------- #
-# >>> >> >  DO NOT MESS WiTH ANYTHiNG BELOW THiS LiNE!  < << <<< #
-# -------------------------------------------------------------- #
 
-# write and check log
 _LOG() {
     if [ -n "$LOGFILE" ]; then
         #check size of logfile
@@ -33,17 +39,9 @@ _LOG() {
 }
 
 apt-get update
-[ ! -z "$LOG" -a "$LOG" == 1 ] && _LOG " upgrade \n$(apt-get upgrade -y)"
-[ ! -z "$LOG" -a "$LOG" == 1 ] && _LOG " autoclean \n$(apt-get autoclean)"
-[ ! -z "$LOG" -a "$LOG" == 1 ] && _LOG " clean $(apt-get clean)"
-[ ! -z "$LOG" -a "$LOG" == 1 ] && _LOG " autoremove \n$(apt-get autoremove -y)"
-#apt upgrade -y
-#apt autoclean
-#apt clean
-#apt autoremove -y
-
-if [ -f /var/run/reboot-required ]; then
-        reboot & exit 0
-fi
+[ ! -z "$LOG" -a "$LOG" == 1 ] && _LOG " upgrade \n$(apt-get upgrade -y)" || apt-get upgrade -y
+[ ! -z "$LOG" -a "$LOG" == 1 ] && _LOG " autoclean \n$(apt-get autoclean)" || apt-get autoclean
+[ ! -z "$LOG" -a "$LOG" == 1 ] && _LOG " clean \n$(apt-get clean)" || apt-get clean
+[ ! -z "$LOG" -a "$LOG" == 1 ] && _LOG " autoremove \n$(apt-get autoremove -y)" || apt-get autoremove -y
 
 exit 0
